5a177027f533daafcd78230d00fd03d23eb7a8b5
Benjamin Williams <benjaminwhitlockwilliams@gmail.com>
2019-04-06 17:32:57 -0500

    Basic project setup with webpack

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const title = 'React Project Boilerplate :)';
+
+    return (
+      <div>{title}</div>
+    );
+  }
+}
+
+export default App;
