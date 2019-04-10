ff5cbb6507f016a8e30a571ee66a17bd4080cc65
jayden-rohwein <jayrohwein@yahoo.com>
2019-04-10 19:57:12 -0400

    playing with Ben's code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loveMeter: 0
+    };
+  }
+
+  onClick = () => {
+    this.setState({ ...this.state, loveMeter: this.state.loveMeter + 10 });
+    //window.alert("we love guy fieri this much" + this.state.loveMeter);
+  };
+
   render() {
-    const title = 'React Project Boilerplate :)';
+    const title = "React Project Boilerplate :(";
 
     return (
-      <div>{title}</div>
+      <div>
+        <div>{title}</div>
+        <span> I love Guy Feiri this much {this.state.loveMeter}/100 </span>
+        <button onClick={this.onClick}> give guy love</button>
+      </div>
     );
   }
 }
